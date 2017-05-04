5c484ac04cd76a91deef3567c90cbf25ea60f0b9
kys10 <skyisno.1@gmail.com>
2017-05-04 14:37:39 +0800

    Change the way to sample from noise
    The NCE module chose one word from vocabulary in equal
    probabilities before, which caused the perplexity stick to around 500
    after 40 epochs. In this commit, the NCE module chooses one word from
    the unigram distribution.
    
    Results: 113.37 epochs after 32 epochs

--- nce.py
+++ nce.py
@@ -1,6 +1,7 @@
 # the NCE module written for pytorch
 
-from numpy.random import randint
+import numpy as np
+from numpy.random import choice
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
@@ -43,7 +44,9 @@ class NCELoss(nn.Module):
         assert input.size(0) == target.size(0)
         data_samples = input.gather(dim=1, index=target.unsqueeze(1))
         data_prob = data_samples.sub(self.norm_term).exp()
-        noise_samples = torch.LongTensor(randint(self.ntokens - 1, size=(target.size(0), self.noise_ratio))) + 1
+        noise_samples = torch.LongTensor(
+            choice(np.arange(0, self.ntokens), p=self.noise.data.numpy(), size=(target.size(0), self.noise_ratio))
+        )
         if self.cuda:
             noise_samples = noise_samples.cuda()
         noise_probs = self.noise[noise_samples.view(-1)].view_as(noise_samples)
