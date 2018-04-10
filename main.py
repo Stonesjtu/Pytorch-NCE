209dad3802344aa80d595773feb8fdd3983b01e1
kys10 <skyisno.1@gmail.com>
2018-04-10 16:43:59 +0800

    Use Adagrad for asgd, fetch/push params per batch

--- main.py
+++ main.py
@@ -57,25 +57,31 @@ logger.info('model definition:\n %s', model)
 
 world_size = distributed.get_world_size()
 rank = distributed.get_rank()
-sync_interval = 100
+sync_interval = 1
 
+import time
 def master(model, data_source, epoch, lr=1.0, weight_decay=1e-5, momentum=0.9):
-    optimizer = optim.SGD(
+    model = model.cuda()
+    optimizer = optim.Adagrad(
         params=model.parameters(),
-        lr=lr,
-        momentum=momentum,
         weight_decay=weight_decay
     )
-    model = model.cuda()
 
     def recv_grad(sender_rank):
         """Receive gradients computed by worker node specified by sender_rank"""
         counter = 0
+        triplets = []
         while True:
-            events = []
             for name, p in model.named_parameters():
                 tensor_buffer = p.new(p.size())
-                distributed.recv(tensor_buffer, sender_rank)
+                event = distributed.irecv(tensor_buffer, sender_rank)
+                triplets.append((name, p, tensor_buffer, event))
+
+            while triplets:
+                name, p, tensor_buffer, event = triplets.pop(0)
+                # waiting for compeletion
+                while not event.is_completed():
+                    time.sleep(0.0005)
                 if p.grad is None:
                     p.grad = tensor_buffer
                 else:
@@ -83,8 +89,7 @@ def master(model, data_source, epoch, lr=1.0, weight_decay=1e-5, momentum=0.9):
             optimizer.step()
             optimizer.zero_grad()
             counter += 1
-            if counter == sync_interval:
-                counter = 0
+            if counter % sync_interval == 0:
                 for p in model.parameters():
                     distributed.send(p.data, sender_rank)
 
@@ -96,9 +101,11 @@ def master(model, data_source, epoch, lr=1.0, weight_decay=1e-5, momentum=0.9):
         t.join()
 
 
+count = 0
 def worker(model, data_source, epoch, lr=1.0, weight_decay=1e-5, momentum=0.9):
     import time
-    with torch.cuda.device(rank):
+    global count
+    with torch.cuda.device(rank - 1):
         model = model.cuda()
         optimizer = optim.SGD(
             params=model.parameters(),
@@ -108,9 +115,9 @@ def worker(model, data_source, epoch, lr=1.0, weight_decay=1e-5, momentum=0.9):
         )
         # Turn on training mode which enables dropout.
         model.train()
-        total_loss = 0
         if rank == 1:
             pbar = tqdm(data_source, desc='Training PPL: ....')
+            #pbar = data_source
         else:
             pbar = data_source
 
@@ -127,40 +134,32 @@ def worker(model, data_source, epoch, lr=1.0, weight_decay=1e-5, momentum=0.9):
             # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
             torch.nn.utils.clip_grad_norm(model.parameters(), args.clip)
 
+            torch.cuda.synchronize()
             for name, p in model.named_parameters():
-                # p.grad.data.zero_()
                 events.append(distributed.isend(p.grad.data, 0))
 
-            optimizer.step()
-
-            total_loss += loss.data[0]
-            if (num_batch+1) % sync_interval == 0 and num_batch > 0:
-
+            count += 1
+            if count % sync_interval == 0:
                 for p in model.parameters():
                     tensor_buffer = p.new(p.size())
                     distributed.recv(tensor_buffer, 0)
-                    p.data.set_(tensor_buffer)
-
-                if rank == 1:
-                    cur_loss = total_loss / sync_interval  #args.log_interval
-                    ppl = math.exp(cur_loss)
-                    pbar.set_description('Training PPL %.1f' % ppl)
-                    total_loss = 0
+                    p.data.set_(tensor_buffer.data)
 
 def evaluate(model, data_source, cuda=args.cuda):
     # Turn on evaluation mode which disables dropout.
-    model.eval()
+    with torch.cuda.device(rank - 1):
+        model.eval()
 
-    eval_loss = 0
-    total_length = 0
+        eval_loss = 0
+        total_length = 0
 
-    for data_batch in data_source:
-        data, target, length = process_data(data_batch, cuda=cuda, sep_target=sep_target)
+        for data_batch in data_source:
+            data, target, length = process_data(data_batch, cuda=cuda, sep_target=sep_target)
 
-        loss = model(data, target, length)
-        cur_length = int(length.data.sum())
-        eval_loss += loss.data[0] * cur_length
-        total_length += cur_length
+            loss = model(data, target, length)
+            cur_length = int(length.data.sum())
+            eval_loss += loss.data[0] * cur_length
+            total_length += cur_length
 
     return math.exp(eval_loss/total_length)
 
@@ -170,18 +169,17 @@ def run_epoch(epoch, lr, best_val_ppl):
     epoch_start_time = time.time()
     if rank != 0:
         worker(model, corpus.train, epoch=epoch, lr=lr, weight_decay=args.weight_decay)
-        return lr, best_val_ppl
-    master(model, corpus.train, epoch=epoch, lr=lr, weight_decay=args.weight_decay)
+    else:
+        master(model, corpus.train, epoch=epoch, lr=lr, weight_decay=args.weight_decay)
     val_ppl = evaluate(model, corpus.valid)
     logger.warn(
-        '| end of epoch {:3d} | time: {:5.2f}s |'
+        'worker {} | end of epoch {:3d} | time: {:5.2f}s |'
         'valid ppl {:8.2f}'.format(
+            rank,
             epoch,
             (time.time() - epoch_start_time),
             val_ppl)
     )
-    with open(args.save+'.epoch_{}'.format(epoch), 'wb') as f:
-        torch.save(model, f)
     # Save the model if the validation loss is the best we've seen so far.
     if not best_val_ppl or val_ppl < best_val_ppl:
         with open(args.save, 'wb') as f:
