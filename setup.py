ccfbe8ebc51d14889bc45e81bc41dc3b33c18706
Michael Graczyk <mgraczyk@users.noreply.github.com>
2019-11-06 05:48:15 -0800

    Add setup.py for pypi packaging (#11)
    Also add hidden files to gitignore

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+install_requires = list(open('requirements.txt').read().splitlines())
+
+setuptools.setup(
+    name='pytorch-nce',
+    version='0.0.1',
+    author='Kaiyu Shi',
+    author_email='skyisno.1@gmail.com',
+    description='An NCE implementation in pytorch',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Stonesjtu/Pytorch-NCE',
+    packages=['nce'],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires,
+)
